e340ec01800aeca78b4e97254e30271edfeceb5c
ribeirotech <contatofelipetarso@gmail.com>
2025-06-22 17:51:10 -0300

    v1 código variável
    Subindo no git a versão 1 do nosso código

---
+++ script.js
@@ -0,0 +1,11 @@
+function armazenarvalor () {
+
+    //armazena o valor que o usuario digitou
+    var userInput = document.getElementById("userInput").value;
+
+    //exibe o valor armazenado
+    console.log("O valor da variavel é:"+ userInput);
+
+    //atualiza o conteudo
+    document.getElementById("valorInserido").innerText = "O valor inserido é: " + userInput;
+}
